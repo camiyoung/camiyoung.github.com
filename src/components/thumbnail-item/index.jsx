22db00042d055808685b0b933b3d3adc26c0d5cf
camiyoung <anji6978@naver.com>
2021-12-01 00:09:44 +0900

    썸네일 변경: 이모지 추가

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
-
+import twemoji from 'twemoji'
 import './index.scss'
 
-export const ThumbnailItem = ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>
-        {node.frontmatter.title || node.fields.slug}
-        <span className="date">{node.frontmatter.date}</span>
-      </h3>
+export const ThumbnailItem = ({ node }) => {
+  const emoji = node.frontmatter.emoji
+  return (
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
+      <div key={node.fields.slug}>
+        <h3>
+          {node.frontmatter.title || node.fields.slug}
+          <span className="date">{node.frontmatter.date}</span>
+        </h3>
 
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-    </div>
-  </Link>
-)
+        <div className="excerpt">
+          <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          <div
+            className="emoji-box"
+            dangerouslySetInnerHTML={{
+              __html: twemoji.parse(emoji || '🧸', {
+                folder: 'svg',
+                ext: '.svg',
+              }),
+            }}
+          ></div>
+        </div>
+      </div>
+    </Link>
+  )
+}
