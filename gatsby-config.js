943df7646963c32fca63b5eee175296039d1d656
camiyoung <anji6978@naver.com>
2021-12-15 23:37:04 +0900

    sitemap추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,8 +96,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://camiyoung.netlify.app',
+        sitemap: 'https://camiyoung.netlify.app/sitemap.xml',
         policy: [
           {
             userAgent: '*',
