72fe91be9c48bf9cce3945212305c1f5613bd54f
camiyoung <anji6978@naver.com>
2021-12-28 03:40:19 +0900

    코드 순서 수정

--- src/html.js
+++ src/html.js
@@ -12,11 +12,11 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
-          {this.props.headComponents}
           <meta
             name="google-site-verification"
             content="M89eu4pTmyceZN5i5Myo6rMRJpL8AEL7dUT4Nv8x5vc"
           />
+          {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
